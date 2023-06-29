e64a8b08a913570cedf53dd5c934ffeb68eee656
Anastasiia Alyaseva <180915an@gmail.com>
2023-06-29 16:25:10 +0200

    added Readme

--- DynamicIslandTimer/ContentView.swift
+++ DynamicIslandTimer/ContentView.swift
@@ -4,25 +4,41 @@ import ActivityKit
 struct ContentView: View {
     
     @State private var currentActivity: Activity<TimerAttributes>?
+    @State private var selectedDuration = 1
+    @State private var isTimerRunning = false
     
     var body: some View {
         VStack {
+            Picker("Select Duration", selection: $selectedDuration) {
+                ForEach(1...60, id: \.self) { minute in
+                    Text("\(minute) min" )
+                }
+            }
+                .labelsHidden()
+                .pickerStyle(.menu)
+            
             Image(systemName: "clock.fill")
                 .resizable()
                 .frame(width: 100, height: 100)
                 .foregroundColor(.blue)
+           
             Button( action: startActivity) {
                 Text("Start Timer")
                     .foregroundColor(.black)
                     .font(.title2)
+            }
+            Button(action: stopActivity) {
+                Text("Stop Timer")
+                    .foregroundColor(.black)
+                    .font(.title3)
             }
         }
         .padding()
     }
     
     func startActivity() {
-        let startDate = Date.now
-        guard let endDate = Calendar.current.date(byAdding: .minute, value: 1, to: startDate) else { return }
+        let startDate = Date()
+        guard let endDate = Calendar.current.date(byAdding: .minute, value: selectedDuration, to: startDate) else { return }
         let interval = startDate...endDate
         
         let attributes = TimerAttributes (actionName: "Delivery")
@@ -33,6 +49,12 @@ struct ContentView: View {
         } catch {
             print(error.localizedDescription)
         }
+    }
+    
+    func stopActivity() {
+//        currentActivity?.cancel()
+        currentActivity = nil
+        isTimerRunning = false
     }
 }
 
